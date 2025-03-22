8966f84df23e196998e9b78944a541add20f8d83
byaw0r <rrzm001@gmail.com>
2025-03-22 03:03:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {  
+    // Блокируем масштабирование при двойном тапе  
+    let lastTouchEnd = 0;  
+    document.addEventListener('touchend', function(event) {  
+        const now = (new Date()).getTime();  
+        if (now - lastTouchEnd <= 300) { // 300ms — интервал для двойного тапа  
+            event.preventDefault();  
+        }  
+        lastTouchEnd = now;  
+    }, false);  
+
+    // Блокируем масштабирование при жестах двумя пальцами  
+    document.addEventListener('touchmove', function(event) {  
+        if (event.scale !== 1) {  
+            event.preventDefault();  
+        }  
+    }, { passive: false });  
+
+    // Переключение на вторую вкладку  
     const tab1 = document.getElementById('tab1');  
     const tab2 = document.getElementById('tab2');  
     const reloadButton = document.getElementById('reloadButton');  
     const audio = document.getElementById('backgroundAudio');  
 
-    // Функция для переключения на вторую вкладку  
     function switchToTab2() {  
         tab1.classList.add('hidden');  
         tab2.classList.remove('hidden');  
         audio.play();  
     }  
 
-    // Переключение на вторую вкладку при нажатии на кнопку "Перезагрузить"  
     reloadButton.addEventListener('click', switchToTab2);  
-
-    // Переключение на вторую вкладку при нажатии на любое место экрана  
     document.addEventListener('click', switchToTab2);  
 });  
